73f92268fb4c167bfdafa0ab47034cb2843db720
Daragh Martin <daragh@daragh.net>
2024-10-22 16:28:19 +0000

    initial projen rc

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,56 @@
 import { cdk } from 'projen';
+import { TrailingComma } from 'projen/lib/javascript';
+
 const project = new cdk.JsiiProject({
-  author: 'ja-daragh',
-  authorAddress: '158112862+ja-daragh@users.noreply.github.com',
+  author: 'daraghmartin',
+  authorAddress: ' 12092354+daraghmartin@users.noreply.github.com',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.4.0',
-  name: 'os',
+  name: '@fluxineer/custom-cdk-projen',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/158112862+ja-daragh/os.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  repositoryUrl: 'https://github.com/fluxineer/projen-cdk',
+  devDeps: ['eslint-plugin-prettier', 'eslint-config-prettier'],
+  peerDeps: ['constructs', 'projen'],
+  keywords: [
+    'aws',
+    'cdk',
+    'projen',
+    'typescript',
+  ],
 });
+
+/**
+ * Add eslint ignore for CODEOWNERS etc
+ */
+[
+  'node_modules',
+  'package-lock.json',
+  'yarn.lock',
+  '*.MD',
+  'CODEOWNERS',
+].forEach((file2Ignore: string) => {
+  project.eslint?.addIgnorePattern(file2Ignore);
+});
+
+/**
+ * Implement prettier from projen
+ *
+ * @remarks
+ *
+ * For Trailing commas see: {@link https://prettier.io/docs/en/options.html}
+ * "all" - Trailing commas wherever possible (including function parameters and calls). To run, JavaScript code formatted this way needs an engine that supports ES2017 (Node.js 8+ or a modern browser) or downlevel compilation. This also enables trailing commas in type parameters in TypeScript (supported since TypeScript 2.7 released in January 2018).
+ * "es5" - Trailing commas where valid in ES5 (objects, arrays, etc.). Trailing commas in type parameters in TypeScript and Flow.
+ * "none" - No trailing commas.
+ */
+project.prettier?.addOverride({
+  files: '*.ts',
+  options: {
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: TrailingComma.ES5,
+    printWidth: 80,
+  },
+});
+
 project.synth();
